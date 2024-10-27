b2a05ae1e0d1dbd303a33a3c1faa13fad2f9bd67
Hardik Majethia <hardikmajethia9529@gmail.com>
2024-10-27 17:59:01 +0000

    kmeans warm start

--- kmeans.py
+++ kmeans.py
@@ -14,7 +14,12 @@ def kmeans(data: DATA, k: int, max_iters: int = 100):
             clusters[closest_centroid].add(row)
 
         # Step 3: Update centroids
-        new_centroids = [clusters[i].mid() for i in range(k)]
+        new_centroids = []
+        for i in range(k):
+            if len(clusters[i].rows) == 0:
+                new_centroids.append(random.choice(data.rows))
+            else:
+                new_centroids.append(clusters[i].mid())
 
         # Check for convergence (if centroids haven't changed, stop)
         if all(data.dist(centroids[i], new_centroids[i]) < 1e-4 for i in range(k)):
@@ -40,30 +45,31 @@ def kmeans(data: DATA, k: int, max_iters: int = 100):
 
 def myfun():
     d = DATA().adds(csv('data/optimize/misc/auto93.csv'))
-    d = d.shuffle()
-    representatives = kmeans(d, 10)
-    d.rows = [row for row in d.rows if row not in representatives]
-    d.rows = representatives + d.rows
+    
     b4 = [d.chebyshev(row) for row in d.rows]
     somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
     rnd = lambda z: z
     scoring_policies = [
     ('exploit', lambda B, R,: B - R),
     ('explore', lambda B, R :  (math.exp(B) + math.exp(R))/ (1E-30 + abs(math.exp(B) - math.exp(R))))]
-    the.label=10
+    the.label=20
     for what,how in scoring_policies:
-        for the.Last in [0, 10, 20, 30, 40]:
+        for the.Last in [20, 30, 40]:
             for the.branch in [False, True]:
                 start = time.time()
                 result = []
                 warm_result = []
                 runs = 0
                 for _ in range(20):
-                    tmp=d.activeLearning(score=how)
-                    tmp1=d.shuffle().activeLearning(score=how)
-                    runs += len(tmp1)
-                    warm_result += [rnd(d.chebyshev(tmp[0]))]
-                    result += [rnd(d.chebyshev(tmp1[0]))]
+                    d = d.shuffle()
+                    c=d.activeLearning(score=how)
+                    representatives = kmeans(d, the.label)
+                    d.rows = [row for row in d.rows if row not in representatives]
+                    d.rows = representatives + d.rows
+                    w=d.activeLearning(score=how)
+                    runs += len(c)
+                    warm_result += [rnd(d.chebyshev(w[0]))]
+                    result += [rnd(d.chebyshev(c[0]))]
 
                 pre=f"cold {what}/b={the.branch}" if the.Last >0 else "cold rrp"
                 tag = f"{pre},{int(runs/20)}"
