2cdebe171d3fbf293e954255835f7111db134347
Hardik Majethia <hardikmajethia9529@gmail.com>
2024-10-27 17:16:02 +0000

    warm start

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,5 @@
 from ezr import the, DATA, csv
-import os
-import random
+import os, random, stats, time, math
 
 def kmeans(data: DATA, k: int, max_iters: int = 100):
     # Step 1: Randomly initialize centroids by selecting k rows from data
@@ -24,13 +23,59 @@ def kmeans(data: DATA, k: int, max_iters: int = 100):
         # Update centroids for the next iteration
         centroids = new_centroids
 
-    return clusters, centroids
+    representatives = []
+    for i, cluster in enumerate(clusters):
+        centroid = centroids[i]
+        min_distance = 1E32
+        representative = None
+        for row in cluster.rows:
+            distance = cluster.dist(row, centroid)
+            if distance < min_distance:
+                min_distance = distance
+                representative = row
+        representatives.append(representative)
+
+    return representatives
 
 
 def myfun():
     d = DATA().adds(csv('data/optimize/misc/auto93.csv'))
-    clusters, centroids = kmeans(d, 10)
-    print(centroids)
+    d = d.shuffle()
+    representatives = kmeans(d, 10)
+    d.rows = [row for row in d.rows if row not in representatives]
+    d.rows = representatives + d.rows
+    b4 = [d.chebyshev(row) for row in d.rows]
+    somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
+    rnd = lambda z: z
+    scoring_policies = [
+    ('exploit', lambda B, R,: B - R),
+    ('explore', lambda B, R :  (math.exp(B) + math.exp(R))/ (1E-30 + abs(math.exp(B) - math.exp(R))))]
+    the.label=10
+    for what,how in scoring_policies:
+        for the.Last in [0, 10, 20, 30, 40]:
+            for the.branch in [False, True]:
+                start = time.time()
+                result = []
+                warm_result = []
+                runs = 0
+                for _ in range(20):
+                    tmp=d.activeLearning(score=how)
+                    tmp1=d.shuffle().activeLearning(score=how)
+                    runs += len(tmp1)
+                    warm_result += [rnd(d.chebyshev(tmp[0]))]
+                    result += [rnd(d.chebyshev(tmp1[0]))]
+
+                pre=f"cold {what}/b={the.branch}" if the.Last >0 else "cold rrp"
+                tag = f"{pre},{int(runs/20)}"
+                print(tag, f": {(time.time() - start) /20:.2f} secs")
+                somes +=   [stats.SOME(result,    tag)]
+
+                pre=f"warm {what}/b={the.branch}" if the.Last >0 else "warm rrp"
+                tag = f"{pre},{int(runs/20)}"
+                print(tag, f": {(time.time() - start) /20:.2f} secs")
+                somes +=   [stats.SOME(warm_result,    tag)]
+
+    stats.report(somes, 0.01)    
 
 #   for folder in os.listdir('data/optimize'):
 #     for file in os.listdir(f'data/optimize/{folder}'):
