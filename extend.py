e14f0babfac750b2d11a6d0d0622f3f61a03ec64
Hardik Majethia <hardikmajethia9529@gmail.com>
2024-10-28 07:09:33 +0000

    outputs

--- extend.py
+++ extend.py
@@ -1,43 +1,38 @@
 import sys, time, math, stats, os
 from ezr import the, DATA, csv
 
-def myfun():
-  cmds = ''
-  for folder in os.listdir('data/optimize'):
-    for file in os.listdir(f'data/optimize/{folder}'):
-      if file.endswith('csv'):
-        d = DATA().adds(csv(file))
-        if len(d.cols.x) > 6:
-          cmds += f'python3.13 extend.py data/optimize/{folder}/{file} > output_highX/{file} &\n'
-        else:
-          cmds += f'python3.13 extend.py data/optimize/{folder}/{file} > output_LowX/{file} &\n'
+cmds = ''
+for folder in os.listdir('data/optimize'):
+  for file in os.listdir(f'data/optimize/{folder}'):
+    if file.endswith('csv'):
+      cmds += f'python3.13 kmeans.py data/optimize/{folder}/{file} > warm_start_outputs/{file} &\n'
 
-  print(cmds)     
+print(cmds)     
 
 
-d = DATA().adds(csv(sys.argv[1]))
-b4 = [d.chebyshev(row) for row in d.rows]
-somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
-rnd = lambda z: z
-scoring_policies = [
-  ('exploit', lambda B, R,: B - R),
-  ('explore', lambda B, R :  (math.exp(B) + math.exp(R))/ (1E-30 + abs(math.exp(B) - math.exp(R))))]
+# d = DATA().adds(csv(sys.argv[1]))
+# b4 = [d.chebyshev(row) for row in d.rows]
+# somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
+# rnd = lambda z: z
+# scoring_policies = [
+#   ('exploit', lambda B, R,: B - R),
+#   ('explore', lambda B, R :  (math.exp(B) + math.exp(R))/ (1E-30 + abs(math.exp(B) - math.exp(R))))]
 
-for what,how in scoring_policies:
-  for the.Last in [0,20, 30, 40]:
-    for the.branch in [False, True]:
-      start = time.time()
-      result = []
-      runs = 0
-      for _ in range(20):
-         tmp=d.shuffle().activeLearning(score=how)
-         runs += len(tmp)
-         result += [rnd(d.chebyshev(tmp[0]))]
+# for what,how in scoring_policies:
+#   for the.Last in [0,20, 30, 40]:
+#     for the.branch in [False, True]:
+#       start = time.time()
+#       result = []
+#       runs = 0
+#       for _ in range(20):
+#          tmp=d.shuffle().activeLearning(score=how)
+#          runs += len(tmp)
+#          result += [rnd(d.chebyshev(tmp[0]))]
 
-      pre=f"{what}/b={the.branch}" if the.Last >0 else "rrp"
-      tag = f"{pre},{int(runs/20)}"
-      print(tag, f": {(time.time() - start) /20:.2f} secs")
-      somes +=   [stats.SOME(result,    tag)]
+#       pre=f"{what}/b={the.branch}" if the.Last >0 else "rrp"
+#       tag = f"{pre},{int(runs/20)}"
+#       print(tag, f": {(time.time() - start) /20:.2f} secs")
+#       somes +=   [stats.SOME(result,    tag)]
 
-stats.report(somes, 0.01)
+# stats.report(somes, 0.01)
 
--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,5 @@
 from ezr import the, DATA, csv
-import os, random, stats, time, math
+import sys, random, stats, time, math
 
 def kmeans(data: DATA, k: int, max_iters: int = 100):
     # Step 1: Randomly initialize centroids by selecting k rows from data
@@ -43,53 +43,41 @@ def kmeans(data: DATA, k: int, max_iters: int = 100):
     return representatives
 
 
-def myfun():
-    d = DATA().adds(csv('data/optimize/misc/auto93.csv'))
-    
-    b4 = [d.chebyshev(row) for row in d.rows]
-    somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
-    rnd = lambda z: z
-    scoring_policies = [
-    ('exploit', lambda B, R,: B - R),
-    ('explore', lambda B, R :  (math.exp(B) + math.exp(R))/ (1E-30 + abs(math.exp(B) - math.exp(R))))]
-    the.label=20
-    for what,how in scoring_policies:
-        for the.Last in [20, 30, 40]:
-            for the.branch in [False, True]:
-                start = time.time()
-                result = []
-                warm_result = []
-                runs = 0
-                for _ in range(20):
-                    d = d.shuffle()
-                    c=d.activeLearning(score=how)
-                    representatives = kmeans(d, the.label)
-                    d.rows = [row for row in d.rows if row not in representatives]
-                    d.rows = representatives + d.rows
-                    w=d.activeLearning(score=how)
-                    runs += len(c)
-                    warm_result += [rnd(d.chebyshev(w[0]))]
-                    result += [rnd(d.chebyshev(c[0]))]
+d = DATA().adds(csv(sys.argv[1]))
 
-                pre=f"cold {what}/b={the.branch}" if the.Last >0 else "cold rrp"
-                tag = f"{pre},{int(runs/20)}"
-                print(tag, f": {(time.time() - start) /20:.2f} secs")
-                somes +=   [stats.SOME(result,    tag)]
+b4 = [d.chebyshev(row) for row in d.rows]
+somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
+rnd = lambda z: z
+scoring_policies = [
+('exploit', lambda B, R,: B - R),
+('explore', lambda B, R :  (math.exp(B) + math.exp(R))/ (1E-30 + abs(math.exp(B) - math.exp(R))))]
+the.label=10
+for what,how in scoring_policies:
+    for the.Last in [20, 30, 40]:
+        for the.branch in [False, True]:
+            start = time.time()
+            result = []
+            warm_result = []
+            runs = 0
+            for _ in range(20):
+                d = d.shuffle()
+                c=d.activeLearning(score=how)
+                representatives = kmeans(d, the.label)
+                d.rows = [row for row in d.rows if row not in representatives]
+                d.rows = representatives + d.rows
+                w=d.activeLearning(score=how)
+                runs += len(c)
+                warm_result += [rnd(d.chebyshev(w[0]))]
+                result += [rnd(d.chebyshev(c[0]))]
 
-                pre=f"warm {what}/b={the.branch}" if the.Last >0 else "warm rrp"
-                tag = f"{pre},{int(runs/20)}"
-                print(tag, f": {(time.time() - start) /20:.2f} secs")
-                somes +=   [stats.SOME(warm_result,    tag)]
+            pre=f"cold {what}/b={the.branch}" if the.Last >0 else "cold rrp"
+            tag = f"{pre},{int(runs/20)}"
+            print(tag, f": {(time.time() - start) /20:.2f} secs")
+            somes +=   [stats.SOME(result,    tag)]
 
-    stats.report(somes, 0.01)    
+            pre=f"warm {what}/b={the.branch}" if the.Last >0 else "warm rrp"
+            tag = f"{pre},{int(runs/20)}"
+            print(tag, f": {(time.time() - start) /20:.2f} secs")
+            somes +=   [stats.SOME(warm_result,    tag)]
 
-#   for folder in os.listdir('data/optimize'):
-#     for file in os.listdir(f'data/optimize/{folder}'):
-#       if file.endswith('csv'):
-#         d = DATA().adds(csv(f'data/optimize/{folder}/{file}'))
-#         clusters, centroids = kmeans(d, 10)
-#         print(centroids, clusters)
-#         break
-
-
-myfun()
+stats.report(somes, 0.01)
